7a0d6c73c9c087da26ee44124a02819d6cf96e8d
spiltcoffee <spiltcoffee@users.noreply.github.com>
2022-12-30 22:13:18 +1100

    whole bunch of stuff

---
+++ src/fontawesome.js
@@ -0,0 +1,15 @@
+import { faExternalLinkSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faMastodon,
+  faTwitter,
+  faDiscord
+} from "@fortawesome/free-brands-svg-icons";
+
+export const icons = {
+  external: faExternalLinkSquare,
+  github: faGithub,
+  mastodon: faMastodon,
+  twitter: faTwitter,
+  discord: faDiscord
+};
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import "./fontawesome.js";
+import "./style.css";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
